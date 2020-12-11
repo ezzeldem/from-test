95b8a16a78c55a7de4b0522319ce81b4946f10d6
ezzelden <ezzelden06@gmail.com>
2020-12-11 04:05:09 +0200

    eezz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("#number-val").on("change", function () {
+        $(".val-show").val($(this).children("option:selected").val());
+    });
 
-        $('#number-val').on('change', function () {
-            $('.val-show').val($(this).children("option:selected").val());
-        });
-
-        $('.add-btn').click(function(){
-            var conn = $(this).attr('span-attr');
-            var val = $('.pro-num-input[input-attr="'+conn+'"]').val();
-            $('.pro-num-input[input-attr="'+conn+'"]').val(parseInt(val) + 1);
-            if( val + 1 >= 1){
-                $('.date-box').show();
-              }
-          });
-          $('.min-btn').click(function(){
-            var conn = $(this).attr('span-attr');
-            var val = $('.pro-num-input[input-attr="'+conn+'"]').val();
-            if( val > 1){
-              $('.pro-num-input[input-attr="'+conn+'"]').val(parseInt(val) - 1);
-            }
-          });
-
+    $(".add-btn").click(function () {
+        var conn = $(this).attr("span-attr");
+        var val = $('.pro-num-input[input-attr="' + conn + '"]').val();
+        $('.pro-num-input[input-attr="' + conn + '"]').val(parseInt(val) + 1);
+        if (val + 1 >= 1) {
+            $(".date-box").show();
+        }
+    });
+    $(".min-btn").click(function () {
+        var conn = $(this).attr("span-attr");
+        var val = $('.pro-num-input[input-attr="' + conn + '"]').val();
+        if (val > 1) {
+            $('.pro-num-input[input-attr="' + conn + '"]').val(parseInt(val) - 1);
+        }
+    });
 });
